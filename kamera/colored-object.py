760ccf10b8a88bf1afeb475844fd0b05c38e11b7
Buaa <vukomans96@yahoo.com>
2017-04-18 17:39:32 +0200

    doradjeni kodovi za kameru

--- kamera/colored-object.py
+++ kamera/colored-object.py
@@ -2,27 +2,71 @@ import cv2
 import numpy as np
 import sys
 from functions import *
+import functions
 import time
 
 PRINT_LOG = False
-
 thresholdColor = {}
-cap = cv2.VideoCapture(0)
+IMG_FRAME = None
+enabledPick = False
+
+
+def mouseCallback(event,x,y,flags,param):
+    global thresholdColor,lowSV,highSV, enabledPick
+
+    if not enabledPick:
+        return
+
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        print '[+] Mouse position: x', x, 'y', y
+        hsv = cv2.cvtColor(IMG_FRAME, cv2.COLOR_BGR2HSV)
+        hsv_picked = hsv[x][y]
+        print '[+] Picked color:', hsv_picked
+
+
+        hsvLow_picked = hsv_picked.copy()
+        hsvHigh_picked = hsv_picked.copy()
+
+        if(hsvLow_picked[0] >= 10):
+            hsvLow_picked[0] -= 10
+        else:
+            hsvLow_picked[0] = 0
+
+        if(hsvHigh_picked[0] <= 245):
+            hsvHigh_picked[0] += 10
+        else:
+            hsvHigh_picked[0] = 255
+
+        hsvLow_picked[1] = hsvLow_picked[2] = lowSV
+        hsvHigh_picked[1] = hsvHigh_picked[2] = highSV
+
+        thresholdColor['lower-color'] = hsvLow_picked
+        thresholdColor['upper-color'] = hsvHigh_picked
+
+        print thresholdColor['lower-color'], thresholdColor['upper-color']
+
+        enabledPick = False
+    return
+
+
+
+calibrateFromFile()
+print functions.OBJECT_RADIUS,functions.CAM_DISTANCE,functions.REAL_OBJECT_WIDTH,functions.REAL_OBJECT_HEIGHT,functions.REAL_OBJECT_DIAM
+
+cap = cv2.VideoCapture(1)
 cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, RESOLUTION[0])
 cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, RESOLUTION[1])
-cap.set(cv2.cv.CV_CAP_PROP_FPS, 15)
+# cap.set(cv2.cv.CV_CAP_PROP_FPS, 15)
+cv2.namedWindow('frame')
+cv2.setMouseCallback('frame',mouseCallback)
+
+
+
 
 if len(sys.argv) > 1:
+
     for i in range(1,len(sys.argv)):
-        if sys.argv[i] == '--low-rgb' and len(sys.argv)>=i+3:
-            thresholdColor['lower-color'] = rgb2hsvColor([sys.argv[i+1], sys.argv[i+2], sys.argv[i+3]])
-        elif sys.argv[i] == '--up-rgb' and len(sys.argv)>=i+3:
-            thresholdColor['upper-color'] = rgb2hsvColor([sys.argv[i+1], sys.argv[i+2], sys.argv[i+3]])
-        elif sys.argv[i] == '--low-hsv' and len(sys.argv)>=i+3:
-            thresholdColor['lower-color'] = [int(sys.argv[i+1]),int(sys.argv[i+2]),int(sys.argv[i+3])]
-        elif sys.argv[i] == '--up-hsv' and len(sys.argv)>=i+3:
-            thresholdColor['upper-color'] = [int(sys.argv[i+1]),int(sys.argv[i+2]),int(sys.argv[i+3])]
-        elif sys.argv[i] == '--rgb' and len(sys.argv)>=i+5:
+        if sys.argv[i] == '--rgb' and len(sys.argv)>=i+5:
             thresholdColor['lower-color'] = rgb2hsvColor([sys.argv[i+1], sys.argv[i+2], sys.argv[i+3]])
             thresholdColor['upper-color'] = rgb2hsvColor([sys.argv[i+1], sys.argv[i+2], sys.argv[i+3]])
             if(thresholdColor['lower-color'][0] >= 10):
@@ -42,44 +86,38 @@ if not thresholdColor.has_key('lower-color') and not thresholdColor.has_key('upp
     thresholdColor = inputColorRGB()
 
 while(True):
-    ret, frame = cap.read()
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lowerColor = np.array(thresholdColor['lower-color'])
-    upperColor = np.array(thresholdColor['upper-color'])
-
-    mask = cv2.inRange(hsv, lowerColor, upperColor)
+    if not enabledPick:
+        ret, IMG_FRAME = cap.read()
+        # IMG_FRAME = cv2.resize(IMG_FRAME,(640, 480), interpolation = cv2.INTER_CUBIC)
+        hsv = cv2.cvtColor(IMG_FRAME, cv2.COLOR_BGR2HSV)
 
 
-    # ret, thresh = cv2.threshold(mask,127,255,0)
-    contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    # result = cv2.bitwise_and(frame, frame, mask=mask)
+        lowerColor = np.array(thresholdColor['lower-color'])
+        upperColor = np.array(thresholdColor['upper-color'])
 
-    # cv2.imshow('frame',frame)
-    # cv2.imshow('result',result)
+        mask = cv2.inRange(hsv, lowerColor, upperColor)
 
-    maxCntIndex = 0
-    maxCntArea = 0
+        contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-    for i in range(0,len(contours)):
-        cntArea = cv2.contourArea(contours[i])
-        if(cntArea > maxCntArea):
-            maxCntIndex = i
-            maxCntArea = cntArea
-    # x = y = w = h = 0
-    circleRadius = 0
+        maxCntIndex = 0
+        maxCntArea = 0
 
-    if(maxCntArea > 0):
-        # x,y,w,h = cv2.boundingRect(contours[maxCntIndex])
-        # cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
+        for i in range(0,len(contours)):
+            cntArea = cv2.contourArea(contours[i])
+            if(cntArea > maxCntArea):
+                maxCntIndex = i
+                maxCntArea = cntArea
+        circleRadius = 0
 
-        (circleX, circleY), circleRadius = cv2.minEnclosingCircle(contours[maxCntIndex])
-        circleRadius = int(circleRadius)
-        circleCenter = (int(circleX),int(circleY))
-        cv2.circle(frame,circleCenter,circleRadius, (0,0,255),3)
-        # cv2.drawContours(frame, contours, maxCntIndex, (0,255,0), 3)
+        if(maxCntArea > 0):
+            (circleX, circleY), circleRadius = cv2.minEnclosingCircle(contours[maxCntIndex])
+            circleRadius = int(circleRadius)
+            circleCenter = (int(circleX),int(circleY))
+            cv2.circle(IMG_FRAME,circleCenter,circleRadius, (0,0,255),3)
+            # cv2.drawContours(frame, contours, maxCntIndex, (0,255,0), 3)
 
-    cv2.imshow('mask',frame)
+        cv2.imshow('frame',IMG_FRAME)
 
     #log
     # print '[+] LC:', thresholdColor['lower-color'], 'UC:', thresholdColor['upper-color'], 'LEN:', len(contours), 'MAX:', maxCntIndex
@@ -96,7 +134,7 @@ while(True):
         saveLog = file(saveNameLog, 'w')
         saveLog.write('[+] CONTOURS: ' + str(len(contours)) + ' MAX: ' + str(maxCntIndex) + ' RADIUS: ' + str(circleRadius))
         saveLog.close()
-        cv2.imwrite(saveNamePic, frame)
+        cv2.imwrite(saveNamePic, IMG_FRAME)
     elif k == ord('m') or k == ord('M'):
         currDistance = measureDistance(circleRadius)
         print '[+] Distance:', currDistance, 'cm'
@@ -107,6 +145,8 @@ while(True):
     elif k == ord('a') or k == ord('A'):
         currAngle, currDirection = measureAngle(circleCenter,circleRadius)
         print '[+] Angle:', currAngle, ' Direction:', currDirection
+    elif k == ord('p') or k == ord('P'):
+        enabledPick = not enabledPick
 
 cap.release()
 cv2.destroyAllWindows()
--- kamera/functions.py
+++ kamera/functions.py
@@ -1,13 +1,16 @@
 import cv2
 import numpy as np
 import math
+import os.path
 
 CAM_DISTANCE = 20
 OBJECT_RADIUS = 1
 REAL_OBJECT_WIDTH = 1
 REAL_OBJECT_HEIGHT = 1
 REAL_OBJECT_DIAM = 1
-RESOLUTION = (800,488)
+RESOLUTION = (640,480)
+lowSV = 100
+highSV = 255
 
 def rgb2hsvColor(rgb):
     bgr = np.uint8([[[rgb[2],rgb[1],rgb[0]]]])
@@ -15,6 +18,8 @@ def rgb2hsvColor(rgb):
     return hsv[0][0]
 
 def inputColorRGB():
+    global lowSV, highSV
+
     print 'Input RBG value color'
     colorR = input('Color R value: ')
     colorG = input('Color G value: ')
@@ -75,6 +80,30 @@ def calibrate(objRadius):
 
     REAL_OBJECT_DIAM = math.sqrt(REAL_OBJECT_WIDTH**2 + REAL_OBJECT_HEIGHT**2)
 
+    calibrationFile = file('calibration.txt','w')
+    calibrationFile.write(str(OBJECT_RADIUS) + ';' + str(CAM_DISTANCE) + ';' + str(REAL_OBJECT_WIDTH) + ';' + str(REAL_OBJECT_HEIGHT) + ';' + str(REAL_OBJECT_DIAM))
+    calibrationFile.close()
+
+def calibrateFromFile():
+    global CAM_DISTANCE, OBJECT_RADIUS, REAL_OBJECT_WIDTH, REAL_OBJECT_HEIGHT, REAL_OBJECT_DIAM
+
+    if(os.path.isfile('calibration.txt')):
+        calibrationFile = file('calibration.txt','r')
+        values = str(calibrationFile.readline()).split(';')
+        calibrationFile.close()
+        if len(values) != 5:
+            print '[!] Invalid calibration file!'
+            return
+
+        OBJECT_RADIUS = float(values[0])
+        CAM_DISTANCE = float(values[1])
+        REAL_OBJECT_WIDTH = float(values[2])
+        REAL_OBJECT_HEIGHT = float(values[3])
+        REAL_OBJECT_DIAM = float(values[4])
+        
+        print '[+] Values from calibration file succesfuly loaded!'
+    else:
+        print '[!] Calibration file not found!'
 
 def measureDistance(currentRadius):
     global CAM_DISTANCE, OBJECT_RADIUS
